fd43476ae8aa3fa4c8bb70db29975e50d4089a05
Dagorik <dagorik@gmail.com>
2018-10-10 19:42:58 -0500

    add repaso

---
+++ Repaso/main.js
@@ -0,0 +1,53 @@
+// var name = 'Juan'
+// var last_name = 'Perex'
+// var second_last_name = 'Martienz'
+// var concatenar = 'Bienvenido ' + name+ ' ' + last_name + ' ' + second_last_name
+// var concatenar = `Bienvenido${name} ${last_name} ${second_last_name}`
+// var x = false;
+// var comparacion = 1 === 1
+// var semaforo = 'amarillo'
+
+// if (semaforo === 'verde'){
+//     console.log('Cruzo');
+// }else if (semaforo === 'amarillo'){
+//     console.log('Corro');
+// }else{
+//     console.log('Me espero');
+// }
+
+// var edad = 20
+// if (edad >= 18 && edad <= 60){
+//     console.log('Eres mayor de edad, puedes pasar al bar')
+// }else{
+//     console.log('Nel')
+// }
+
+// var ife = "si"
+// var licencia = "si"
+// if(ife === "si" || licencia === "si"){
+//     console.log('Entra al bar')
+// }else{
+//     console.log('Nel')
+// }
+
+//Quiere imprimir la tabla del 5
+// var x = 8
+// for(var i = 0; i <= 10; i++){
+//     console.log(`${x} * ${i} =  ${x*i}`)
+// }
+
+
+// if (num1 === num2 && num1 === num3 && num2 === num3) {
+//     console.log('Son iguales los 3');
+// } else if (num1 === num2 || num1 === num3 || num2 === num3) {
+//     console.log('Hay 2 iguales');
+// } else {
+//     console.log('Todos son diferentes');
+// }
+
+// function sumaDosNumeros(num1,num2){
+//     var total = num1 + num2
+//     return total
+// }
+// var result = sumaDosNumeros(2,2)
+// console.log('-->' , result)
