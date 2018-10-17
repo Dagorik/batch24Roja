ece4a3cc625f9d4aba718daad3a3e00d6fddcdc5
Dagorik <dagorik@gmail.com>
2018-10-16 21:47:27 -0500

    add async methods

---
+++ async.js
@@ -0,0 +1,12 @@
+// console.log(1)
+// setTimeout(()=>{
+//     console.log(2)
+// },3000)
+// console.log(3)
+
+console.log(1)
+setTimeout(()=>{
+    console.log(2)
+},1000)
+for(let i = 0; i<9999999999; i++);
+console.log(3)
